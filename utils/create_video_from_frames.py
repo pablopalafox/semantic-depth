1e54ef07d946c04ef0eea915e05f0f70a7b8d1bc
pablorpalafox <pablorpalafox@gmail.com>
2019-03-19 12:07:42 +0100

    modified create video from frame

--- utils/create_video_from_frames.py
+++ utils/create_video_from_frames.py
@@ -9,7 +9,7 @@ output_path = "results/stuttgart_video/result.mp4"
 test_frame = cv2.imread("media/videos/stuttgart_video/stuttgart_02_000000_005102_leftImg8bit.png")
 height, width = test_frame.shape[0], test_frame.shape[1]
 
-video = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"MJPG"), 8, (width, height))
+video = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"mp4v"), 30, (width, height))
 
 
 for frame_path in sorted(glob.glob(input_path)):
